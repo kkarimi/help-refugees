bce14c43ae4c663fb32daaf873330942f8641e10
🇵🇷 <cj@ponti.io>
2017-08-07 18:35:48 +0100

    feat(UserForm): Create UserForm file

---
+++ src/components/UserForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class UserForm extends Component {
+  state = {
+
+  }
+
+  // constructor (props) {
+  //   super(props)
+  // }
+
+  render () {
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+
+        </div>
+        <div className="panel-body">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default UserForm
