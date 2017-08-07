0efe54651179d2a92355d46202cb8419fc749c7e
🇵🇷 <cj@ponti.io>
2017-08-07 19:24:02 +0100

    feat(InputField): Remove handleChange
    It is much better to have the onChange handled by the parent component

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -5,7 +5,6 @@ export default ({ id, name, label, handleChange, type, value, ...props }) => (
     id={id || name}
     type={type || 'text'}
     className="form-control"
-    onChange={(evt) => handleChange(name, evt.target.value)}
     value={value || ''}
     {...props}
   />
