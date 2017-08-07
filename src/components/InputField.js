1ab2490f0abb0b350650383fa89574e3f8250439
🇵🇷 <cj@ponti.io>
2017-08-07 19:26:58 +0100

    feat(InputField): Add name property
    Because the `name` prop was deconstructed in order to potentially use it
    as the field's `id` property, we must assign it manually since it will not be
    present in `{...props}`

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default ({ id, name, label, handleChange, type, value, ...props }) => (
   <input
-    id={id || name}
+    id={id || name} // If the user does not supply an id, use the name of the field
+    name={name}
     type={type || 'text'}
     className="form-control"
     value={value || ''}
