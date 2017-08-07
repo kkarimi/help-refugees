f878f9736dd7c87b49861ef2a4a1db69fcf2986e
🇵🇷 <cj@ponti.io>
2017-08-07 18:35:00 +0100

    test(LogIn): Add test file for LogIn

---
+++ src/components/LogIn.spec.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { shallow } from 'enzyme'
+import { expect } from 'chai'
+import LogIn from './LogIn'
+
+describe('<Login/>', () => {
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <BrowserRouter>
+        <LogIn>
+          <div className="unique" />
+        </LogIn>
+      </BrowserRouter>
+    ))
+
+    expect(wrapper.contains(<div className="unique" />)).to.equal(true)
+  })
+})
