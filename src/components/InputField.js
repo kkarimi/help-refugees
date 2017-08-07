6bf34464fd98a356c1c2dd0d6b3f2d13d9bd3067
🇵🇷 <cj@ponti.io>
2017-08-07 15:50:46 +0100

    fix(InputField): Set value to empty string if none provided
    Have to set value to empty string if none provided because if you do not
    do so, React will complain about switching between a controlled and
    uncontrolled input

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export default ({ id, name, label, handleChange, type, ...props }) => (
+export default ({ id, name, label, handleChange, type, value, ...props }) => (
   <input
     id={id || name}
     type={type || 'text'}
     className="form-control"
     onChange={(evt) => handleChange(name, evt.target.value)}
+    value={value || ''}
     {...props}
   />
 )
