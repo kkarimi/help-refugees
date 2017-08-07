ecaaf979950a21a0ec98a4beffbf53a3e0e231d4
🇵🇷 <cj@ponti.io>
2017-08-07 15:55:34 +0100

    feat(Organisations): Add new features to Organisations list...
    * Add ability to Validate and Delete organisation if logged in user is admin
    * Add updateOrganisations() which calls for newest state of organisations
    * Add "New" button which takes user to empty form
    * Add "Last Updated By" column which displays the user who last updated the org
    * Use "label label-default" styling for service types

--- src/components/Organisations.js
+++ src/components/Organisations.js
@@ -1,27 +1,93 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import moment from 'moment'
 import Callback from '../Callback/Callback'
 
 class Organisations extends PureComponent {
   state = {
-    organisations: {}
+    organisations: {},
+    isLoading: true
   }
 
-  componentWillMount () {
+  static propTypes = {
+    admin: PropTypes.bool.isRequired
+  }
+
+  constructor (props) {
+    super(props)
+
+    // Bind function scopes to class
+    this.validateRecord = this.validateRecord.bind(this)
+    this.deleteRecord = this.deleteRecord.bind(this)
+  }
+
+  validateRecord (org) {
+    const ref = this.props.db.ref(`organisations/${org.uid}`)
+
+    ref
+      .update({ valid: true })
+      .then(() => {
+        this.updateOrganisations()
+      })
+      .catch(() => {
+
+      })
+  }
+
+  /**
+   * Delete Record
+   * @param {Object} org Organisaton to be deleted
+   */
+  deleteRecord (org) {
+    const ref = this.props.db.ref(`organisations/${org.uid}`)
+
+    ref
+      .remove()
+      .then(() => {
+        this.updateOrganisations()
+      })
+      .catch(() => {
+
+      })
+  }
+
+  updateOrganisations () {
+    this.setState({ isLoading: true })
+
     const ref = this.props.db.ref().child('organisations')
 
     ref.once('value', snap => {
-      this.setState({ organisations: snap.val() || {} })
+      this.setState({
+        isLoading: false,
+        organisations: snap.val() || {}
+      })
     })
   }
 
+  componentWillMount () {
+    this.updateOrganisations()
+  }
+
   render () {
-    const { organisations } = this.state
+    const { organisations, isLoading } = this.state
+    const { admin } = this.props
+    const uids = Object.keys(organisations)
 
     return (
-      <div className="col-sm-8 col-sm-offset-2">
+      <div className="col-sm-12">
         <div className="panel panel-default">
           {/* <!-- Default panel contents --> */}
-          <div className="panel-heading">Organisations</div>
+          <div className="panel-heading" style={{height: '55px', fontSize: '2.4rem'}}>
+            Organisations
+            <div className="pull-right">
+              <button
+                className="btn btn-default"
+                onClick={() => this.props.history.push('/form')}
+              >
+                New
+              </button>
+            </div>
+          </div>
 
           <div className="panel-body">
             <table className="table">
@@ -31,24 +97,33 @@ class Organisations extends PureComponent {
                   <th>Expiry Date</th>
                   <th>Name</th>
                   <th>Type</th>
+                  <th>Last Updated By</th>
                 </tr>
               </thead>
               <tbody>
                 {
-                  Object.keys(organisations).length === 0
+                  /* TODO Replace with "isLoading" */
+                  isLoading
                   ? <tr><td colSpan="4"><Callback/></td></tr>
                   : (
-                    Object.keys(organisations).map((k, i) => {
-                      const org = organisations[k]
+                    uids.map((uid, i) => {
+                      const org = { ...organisations[uid], uid: uid }
 
                       return (
                         <tr key={i}>
-                          <td>{org.last_updated}</td>
-                          <td>{org.expired}</td>
+                          <td>{moment(org.updated).format('MMM Do YY')}</td>
+                          <td>{moment(org.expiry).format('MMM Do YY')}</td>
                           <td>{org.organisation_name}</td>
                           <td>{
                             org.service_type
-                            ? org.service_type.map((s, i) => <div key={i}>{s}</div>)
+                            ? org.service_type.map((s, i) => (
+                                <div
+                                  key={i}
+                                  className="label label-default"
+                                  style={{ display: 'block', marginBottom: '0.5rem' }}>
+                                  {s}
+                                </div>
+                              ))
                             : null
                           }</td>
                           <td>
@@ -60,6 +135,31 @@ class Organisations extends PureComponent {
                             >
                               Edit
                             </button>
+                            {
+                              admin &&
+                              (
+                                <button
+                                  className="btn btn-default"
+                                  onClick={this.validateRecord.bind(this, org)}
+                                >
+                                  Validate
+                                </button>
+                              )
+                            }
+                            {
+                              admin &&
+                              (
+                                <button
+                                  className="btn btn-default"
+                                  onClick={this.deleteRecord.bind(this, org)}
+                                >
+                                  Delete
+                                </button>
+                              )
+                            }
+                          </td>
+                          <td>
+                            {org.updated_by}
                           </td>
                         </tr>
                       )
