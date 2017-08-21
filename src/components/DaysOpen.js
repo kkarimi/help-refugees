55eed030b98cae379259a7e8e420f1cba8ed36d2
🇵🇷 <cj@ponti.io>
2017-08-21 15:03:42 +0100

    feat(DaysOpen): Complete functionality of DaysOpen

--- src/components/DaysOpen.js
+++ src/components/DaysOpen.js
@@ -4,16 +4,37 @@ import InputField from './InputField'
 class DaysOpen extends Component {
   constructor (props) {
     super(props)
-    this.state = { value: props.value }
+    this.state = props.value || {}
+    this.onOpenChange = this.onOpenChange.bind(this)
+    this.onCloseChange = this.onCloseChange.bind(this)
   }
+
   onChange (name, { target: { checked } }) {
-    const value = { ...this.state.value, [name]: checked }
-    this.setState({ value })
-    this.props.onChange(value)
+    const value = { [name]: checked === true ? {} : void 0 }
+
+    this.setState(value)
+
+    this.props.onChange({ ...this.state, ...value })
+  }
+
+  onOpenChange (name, evt) {
+    const value = { [name]: { ...this.state[name], open: evt.target.value } }
+
+    this.setState(value)
+
+    this.props.onChange({ ...this.state, ...value })
+  }
+
+  onCloseChange (name, evt) {
+    const value = { [name]: { ...this.state[name], close: evt.target.value } }
+
+    this.setState(value)
+
+    this.props.onChange({ ...this.state, ...value })
   }
 
   render () {
-    const { value = {} } = this.state
+    const value = this.state
 
     const days = [
        { name: 'monday', label: 'Monday' },
@@ -26,19 +47,44 @@ class DaysOpen extends Component {
     ]
 
     return (
-      <div>
+      <div className="col-sm-12" style={{ paddingLeft: 0, paddingRight: 0 }}>
         {days.map(({ name, label }, i) => (
-          <div className="checkbox" key={i}>
-            <label htmlFor={`daysOpen.${name}`}>
-              <InputField
-                type="checkbox"
-                id={`daysOpen.${name}`}
-                name={`daysOpen.${name}`}
-                checked={value[name]}
-                onChange={this.onChange.bind(this, name)}
-              />
-              {label}
-            </label>
+          <div className="row" key={i}>
+            <div className="checkbox col-sm-5">
+              <label htmlFor={`daysOpen.${name}`}>
+                <InputField
+                  type="checkbox"
+                  id={`daysOpen.${name}`}
+                  name={`daysOpen.${name}`}
+                  checked={value[name]}
+                  onChange={this.onChange.bind(this, name)}
+                />
+                {label}
+              </label>
+            </div>
+            {
+              value[name] &&
+              (
+                <div className="col-sm-7">
+                  <InputField
+                    type="time"
+                    className="col-sm-6"
+                    id={`daysOpen.${name}.open`}
+                    name={`daysOpen.${name}.open`}
+                    value={value[name].open}
+                    onChange={this.onOpenChange.bind(this, name)}
+                  />
+                  <InputField
+                    type="time"
+                    className="col-sm-6"
+                    id={`daysOpen.${name}.close`}
+                    name={`daysOpen.${name}.close`}
+                    value={value[name].close}
+                    onChange={this.onCloseChange.bind(this, name)}
+                  />
+                </div>
+              )
+            }
           </div>
         ))}
       </div>
