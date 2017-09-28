55cb68929688f9d5ae53118860ada6a250d1efa5
🇵🇷 <cj@ponti.io>
2017-09-28 11:00:31 +0100

    refactor(OrganisationForm): Move unused inputs out of file

--- src/components/OrganisationForm.js
+++ src/components/OrganisationForm.js
@@ -265,32 +265,6 @@ class NewRecord extends PureComponent {
                     </div>
                   </FormGroup>
 
-                  {/*
-                    <div className="form-group">
-                      <label htmlFor="opening_hours">Opening Hours</label>
-                      <InputField name="opening_hours" onChange={this.onFieldChange} />
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="intended_service_user">
-                        Intended Service User (Refugees; Asylum Seekers; Volunteers; etc)
-                      </label>
-                      <InputField name="intended_service_user" onChange={this.onFieldChange}/>
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="nationalities_served"> Nationalities served </label>
-                      <InputField name="nationalities_served" onChange={this.onFieldChange} />
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="limitations">
-                        Limitations/ Capacity (e.g very small group/ waiting time 6 weeks)
-                      </label>
-                      <InputField name="limitations" onChange={this.onFieldChange} />
-                    </div>
-                  */}
-
                   <FormGroup>
                     <Label inputName="volunteer_need"> Volunteer Need </Label>
                     <FormInput>
---
+++ src/components/OrganisationFormInputs.js
@@ -0,0 +1,20 @@
+/**
+    <div className="form-group">
+      <label htmlFor="intended_service_user">
+        Intended Service User (Refugees; Asylum Seekers; Volunteers; etc)
+      </label>
+      <InputField name="intended_service_user" onChange={this.onFieldChange}/>
+    </div>
+
+    <div className="form-group">
+      <label htmlFor="nationalities_served"> Nationalities served </label>
+      <InputField name="nationalities_served" onChange={this.onFieldChange} />
+    </div>
+
+    <div className="form-group">
+      <label htmlFor="limitations">
+        Limitations/ Capacity (e.g very small group/ waiting time 6 weeks)
+      </label>
+      <InputField name="limitations" onChange={this.onFieldChange} />
+    </div>
+ */
