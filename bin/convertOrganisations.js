777f03d0948250d2f93c66e2277a9cbf8ec4fdba
🇵🇷 <cj@ponti.io>
2017-10-13 12:25:12 +0100

    config(google): Move organisations update logic to bin
    * Use Google Sheets Api to get most current version organisations

---
+++ bin/convertOrganisations.js
@@ -0,0 +1,147 @@
+// const path = require('path')
+const _ = require('lodash')
+const winston = require('winston')
+const formatOpeningHours = require('../src/DateTools/formatOpeningHours')
+const getFromGoogleSheets = require('./getFromGoogleSheets')
+
+// var admin = require('firebase-admin')
+// var serviceAccount = require('../firebaseServiceAccount.json')
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://stone-nucleus-173311.firebaseio.com'
+// })
+
+// const db = admin.database() // the real-time database
+
+// const ref = db.ref().child('organisations')
+
+// let count = 0
+
+const headingMapping = {
+  'ov': 'updated_by',
+  'Last updated (DD/MM/YY)': 'updated',
+  'Expiry Date?': 'expiry',
+  'Name': 'name',
+  'Service Type': 'types',
+  'Additional/other (separate each type with a semi-colon)': 'additionalInfo',
+  'Details (who, what, when, where, why)': 'details',
+  'Region': 'region',
+  'City/ Town': 'city',
+  'Address': 'address',
+  'Post Code': 'postCode',
+  'Phone': 'phone',
+  'URL': 'url',
+  'Email': 'email',
+  'Hours': 'openingHours',
+  'Volunteer need?': 'volunteerNeed'
+}
+
+// Get latest data from Google Spreadsheet
+getFromGoogleSheets('1cgkkCwMo70T36Prkhxbj0DWiSLgDahUCWhS63O4_40k', 'Old')
+  .then(function (data) {
+    return data.map(function (obj) {
+      // Convert spreadsheet record to Firebase record
+      return Object.keys(obj).reduce((converted, h) => {
+        const heading = headingMapping[h]
+
+        // Do not return value if there is none
+        if (obj[h].length === 0) return converted
+
+        switch (heading) {
+          case 'types':
+            // convert type to array
+            converted[heading] = obj[h].split(';')
+            break
+          case 'additionalInfo':
+            converted.types = converted.types.concat(obj[h].replace('\n', '').split('; '))
+            break
+          case 'expiry':
+          case 'updated':
+            // Convert `expiry` and `updated` to Dates
+            converted[heading] = !isNaN(new Date(obj[h])) ? new Date(obj[h]) : null
+            break
+          case 'openingHours':
+            converted[heading] = formatOpeningHours(obj[h])
+            break
+          default:
+            // `Trim` text to removing beginning and trailing whitespace
+            converted[heading] = obj[h].trim()
+        }
+
+        if (converted.updated === void 0) converted.status = 'needs_review'
+        else if (converted.updated !== void 0 && converted.expiry === void 0) converted.status = 'in_progress'
+        else if (converted.expiry === void 0) converted.status = 'needs_review'
+        // TODO Add something here assign a expiry of 90 days from now using moment
+
+        if (converted.status === void 0) converted.status = 'needs_review'
+
+        if (converted[heading] === 'No') converted[heading] = false
+        else if (converted[heading] === 'Yes') converted[heading] = true
+
+        return converted
+      }, {})
+    })
+  })
+  .then(function (converted) {
+    return converted.map(org => {
+      org.types = _.isArray(org.types) && org.types.map(t => {
+        switch (t) {
+          case 'Children and Young People':
+            return 'Children & Young People'
+          case 'Destitution':
+            return 'Destitution & Debt'
+          case 'Employment/ Training/ Volunteering':
+          case 'Employment/ Training /Volunteering':
+            return 'Employment / Training / Volunteering'
+          case 'Health':
+          case 'Healthcare/ Mental Health / Disability':
+            return 'Healthcare / Mental Health / Disability'
+          case 'Social':
+          case 'Social (groups, clubs)':
+            return 'Social (clubs, groups)'
+          default:
+            return t
+        }
+      })
+
+      return org
+    })
+  })
+  .then(function (converted) {
+    console.log(converted[0])
+    winston.info(`Number of records converted: ${converted.length}`)
+  })
+//   })
+//   .on('done', function () {
+//     ref
+//       .remove()
+//       .then(function () {
+//         Promise
+//           .all(organisations.map(o => ref.push().set(o)))
+//           .then(() => {
+//             console.log(`${count} organisations added`)
+//             process.exit()
+//           })
+//           .catch(() => {
+//             console.log('Not all organisations added')
+//             process.exit()
+//           })
+//       })
+//       .catch(function () {
+//         console.log('data could not be deleted')
+//         process.exit()
+//       })
+//   })
+
+// function updateRecord (record) {
+//   ref
+//     .orderByChild('name')
+//     .equalTo(record.name)
+//     .on('value')
+//     .then(function (snapshot) {
+//       snapshot.forEach(function (childSnapshot) {
+//         childSnapshot.update(record).then(_ => console.log(`${record.name} has been updated`))
+//       })
+//     })
+// }
