a72586b912191c588b6603a4345b93bf5790c1ef
Charles Ponti <cj@ponti.io>
2017-12-03 17:52:17 +0000

    feat(Home): Use Promise in test

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -2,32 +2,34 @@ import React from 'react'
 import { mount } from 'enzyme'
 import Home from './Home'
 
-const db = {
-  ref () {
-    return {
-      once (p, cb) {
-        cb({
-          val () {
-            return {
-              foo: { name: 'foo' },
-              bar: { name: 'bar' }
-            }
-          }
-        })
-
+describe('<Home/>', function () {
+  test('should render organisations', function () {
+    const db = {
+      ref () {
         return {
-          then: () => {},
-          catch: () => {}
+          once () {
+            return new Promise(function (resolve) {
+              resolve({
+                val: () => ({
+                  foo: { name: 'foo' },
+                  bar: { name: 'bar' }
+                })
+              })
+            })
+          }
         }
       }
     }
-  }
-}
-
-describe('<Home/>', function () {
-  test('should render organisations', function () {
     const element = mount(<Home db={db}/>)
-    expect(element.find('li').at(0).text()).toEqual(' foo ')
-    expect(element.find('li').at(1).text()).toEqual(' bar ')
+
+    // Test that loading spinner is shown before promise is resolved
+    expect(element.find('[alt="loading"]').length).toEqual(1)
+
+    // Must wrap in setTimeout otherwise the Promise would be executed
+    // after these two elements have been run
+    setTimeout(() => {
+      expect(element.find('li').at(0).text()).toEqual(' foo ')
+      expect(element.find('li').at(1).text()).toEqual(' bar ')
+    }, 1)
   })
 })
